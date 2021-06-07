d52a3af59ecdfb70b1fc605b42f679ca4498648a
Ubaidillah Ahmad <a.ubai25@gmail.com>
2021-06-07 22:00:43 +0700

    Initial Commit

---
+++ FruitsyApp/FruitsyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsyAppApp.swift
+//  FruitsyApp
+//
+//  Created by Ahmad Ubaidillah on 07/06/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitsyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
